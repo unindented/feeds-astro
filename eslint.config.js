0f2a773a405e8d9a043e6d48aa0ba4bb6952c9ed
Daniel Perez Alvarez <daniel@unindented.org>
2025-02-12 09:23:03 -0800

    Add eslint-plugin-simple-import-sort

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,8 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginAstro from "eslint-plugin-astro";
+import eslintPluginSimpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -10,4 +11,13 @@ export default [
   pluginJs.configs.recommended,
   eslintConfigPrettier,
   ...eslintPluginAstro.configs.all,
+  {
+    plugins: {
+      "simple-import-sort": eslintPluginSimpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
 ];
